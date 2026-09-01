c0250e09402da3d94a48983ba37b13f871e08591
DenisNikolsky <denis.nikolsky@gmail.com>
2026-09-01 10:45:23 +0300

    feat: add postgres connection

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,17 +1,28 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
+	"github.com/DenisNikolsky/url-shortener/internal/config"
+	"github.com/DenisNikolsky/url-shortener/internal/repository"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
+	cfg := config.Load()
+
+	db, err := repository.NewPostgresDB(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
 	e := echo.New()
 
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
 
-	e.Logger.Fatal(e.Start(":8080"))
+	e.Logger.Fatal(e.Start(":" + cfg.ServerPort))
 }
--- internal/repository/postgres.go
+++ internal/repository/postgres.go
@@ -1,1 +1,31 @@
 package repository
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/DenisNikolsky/url-shortener/internal/config"
+	_ "github.com/jackc/pgx/v5/stdlib"
+)
+
+func NewPostgresDB(cfg config.Config) (*sql.DB, error) {
+	dsn := fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s",
+		cfg.DBUser,
+		cfg.DBPassword,
+		cfg.DBHost,
+		cfg.DBPort,
+		cfg.DBName,
+	)
+
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, fmt.Errorf("open database: %w", err)
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, fmt.Errorf("ping database: %w", err)
+	}
+
+	return db, nil
+}
